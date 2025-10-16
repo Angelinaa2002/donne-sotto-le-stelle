e9de1246d0b60d3fdb59703c3648a4a7ba089496
Angelinaa2002 <prokhorenkoangelina67@gmail.com>
2025-10-16 12:46:16 +0200

    Update NavBar.js

--- donne-sotto-le-stelle-/components/NavBar.js
+++ donne-sotto-le-stelle-/components/NavBar.js
@@ -4,7 +4,7 @@ import styles from "./NavBar.module.css";
 export default function NavBar() {
   return (
     <nav className={styles.nav}>
-      <a href="#chi-sono" className={styles.link}>Chi sono</a>
+      <a href="#chi-siamo" className={styles.link}>Chi siamo</a>
       <a href="#servizi" className={styles.link}>Servizi</a>
       <a href="#prenota" className={styles.link}>Prenota</a>
     </nav>
